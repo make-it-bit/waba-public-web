a53c6db3eef5ca07f9be4ad2a0ac37ea1b65f17b
ketlyiouriev <ketly.iouriev4@gmail.com>
2023-12-08 00:10:09 +0200

    hero & colors section update

--- page-components/Index/Colors/Colors.tsx
+++ page-components/Index/Colors/Colors.tsx
@@ -2,7 +2,11 @@
 
 import React from "react";
 import { useRouter } from "next/navigation";
+import classNames from "classnames";
+
 import { Button } from "../../../gui-components/client";
+
+import styles from "./_colors.module.scss";
 
 const Colors = () => {
   const router = useRouter();
@@ -10,31 +14,43 @@ const Colors = () => {
   return (
     <div className="container">
       <div className="grid grid-cols-12 mb-88">
-        <div className="col-start-4 col-end-10 text-center mt-160 mb-144">
-          <h1 className="text-4xl text-black-100">
+        <div className="col-start-5 col-span-4 text-center mt-160 mb-144">
+          <h1 className="font-rufina text-4xl leading-4xl text-black-100">
             Not just different colors, changeable heads instead
           </h1>
         </div>
-        <div className="col-start-1 col-span-4 flex flex-col justify-between">
+        <div className="col-start-1 col-span-4 flex flex-col justify-between text-black-100">
           <div className="mb-48">
-            <p className="text-sm">Blue head</p>
-            <p className="text-xl">
+            <p className="text-sm leading-sm">Blue head</p>
+            <p className="font-rufina text-xl leading-xl">
               Removes skin imperfections - spots, inflammation and blemishes
             </p>
           </div>
           <div className="mb-48">
-            <p className="text-sm">Red head</p>
-            <p className="text-xl">
+            <p className="text-sm text-sm leading-sm">Red head</p>
+            <p className="font-rufina text-xl leading-xl">
               Reduces fine lines + wrinkles and firm the skin
             </p>
           </div>
           <div>
-            <p className="text-sm">Infrared head</p>
-            <p className="text-xl">Revitalise and balance skin tone</p>
+            <p className="text-sm text-sm leading-sm">Infrared head</p>
+            <p className="font-rufina text-xl leading-xl">
+              Revitalise and balance skin tone
+            </p>
           </div>
         </div>
+        <div className="col-start-5 col-span-4 flex justify-center text-center">
+          <p
+            className={classNames(
+              "text-8xl leading-8xl mt-56",
+              styles.textBackground
+            )}
+          >
+            Blue head
+          </p>
+        </div>
         <div className="col-start-9 col-span-5 flex flex-col justify-between">
-          <p className="text-sm mb-56">
+          <p className="text-sm leading-sm mb-56">
             Blue Light therapy focuses on achieving a clearer and more
             harmonious complexion by specifically targeting common skin concerns
             such as spots, inflammation, and blemishes. This unique wavelength
--- page-components/Index/Hero/Hero.tsx
+++ page-components/Index/Hero/Hero.tsx
@@ -25,17 +25,14 @@ const Hero = () => {
           autoPlay
           muted
           loop
-          className={classNames(
-            "absolute top-0 left-248 w-full h-full",
-            styles.video
-          )}
+          className="absolute lg:top-0 top-400 lg:left-248 left-0 w-full h-full object-cover mix-blend-darken lg:z-[-1]"
         >
           <source src="/hero-video.mp4" type="video/mp4" />
         </video>
-        <div className="grid grid-cols-12">
-          <div className="col-start-1 col-end-7 flex flex-col mt-248 mb-104">
+        <div className="lg:grid grid-cols-12">
+          <div className="col-start-1 col-end-7 flex flex-col lg:items-start items-center lg:text-left text-center lg:mt-248 mt-56 mb-104">
             <div>
-              <h1 className="font-rufina text-7xl leading-7xl mb-32">
+              <h1 className="font-rufina lg:text-7xl text-5xl lg:leading-7xl leading-5xl mb-32">
                 Discover Liberating Beauty
               </h1>
               <h2 className="text-base leading-base">
@@ -46,7 +43,7 @@ const Hero = () => {
                 It's a gateway to a freer, more confident you.
               </h2>
             </div>
-            <div className="flex mt-48 mb-88 gap-8">
+            <div className="flex lg:mt-48 lg:mb-88 my-40 gap-8">
               <Button CTA="Shop now" onClick={() => router.push("#")} />
               <Button
                 CTA="Learn more"
@@ -55,7 +52,7 @@ const Hero = () => {
                 svg={false}
               />
             </div>
-            <div className="flex gap-8">
+            <div className="flex lg:justify-start justify-center flex-wrap gap-8">
               <Tag text="Made in EU" svg />
               <Tag text="CE Certified" />
               <Tag text="2-year warranty" />
