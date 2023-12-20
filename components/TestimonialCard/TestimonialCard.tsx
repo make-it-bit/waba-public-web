2e796f7426d93388933006587ccbfd3855a4d81f
ketlyiouriev <ketly.iouriev4@gmail.com>
2023-12-21 01:28:03 +0200

    update

--- components/TestimonialCard/TestimonialCard.tsx
+++ components/TestimonialCard/TestimonialCard.tsx
@@ -4,7 +4,23 @@ import Link from 'next/link';
 
 import { Button } from '../../gui-components/client';
 
-const TestimonialCard = ({ image, name, border = false, content, buttonCTA = null, resultImage = null }) => {
+type TestimonialCardProps = {
+  image: string;
+  name: string;
+  border?: boolean;
+  content: string;
+  buttonCTA?: string | null;
+  resultImage?: string | null;
+};
+
+const TestimonialCard = ({
+  image,
+  name,
+  border = false,
+  content,
+  buttonCTA = null,
+  resultImage = null,
+}: TestimonialCardProps) => {
   return (
     <div className="bg-white-100 flex flex-col justify-between px-40 py-32 h-full">
       <div className="flex flex-col gap-24 mb-24">
