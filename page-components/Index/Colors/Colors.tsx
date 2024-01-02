42e5402f9d185f5abcd25a980c8a61313c88fb2b
Sebastian Pikand <sebastianpikand@gmail.com>
2024-01-03 00:11:17 +0200

    added timeout scroll effect to device heads

--- page-components/Index/Colors/Colors.tsx
+++ page-components/Index/Colors/Colors.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect, useMemo } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import classNames from 'classnames';
@@ -10,6 +10,21 @@ import { Button } from '@/gui-components/client';
 import styles from './_colors.module.scss';
 
 const Colors = ({ colorsData }) => {
+  const [isIntervalRunning, setIsIntervalRunning] = useState(true);
+  const deviceHeadsListRef = useRef<HTMLDivElement>(null);
+  const blueHeadRef = useRef<HTMLDivElement>(null);
+  const redHeadRef = useRef<HTMLDivElement>(null);
+  const infraredHeadRef = useRef<HTMLDivElement>(null);
+  const refMap = useMemo(
+    () => ({
+      1: blueHeadRef,
+      2: redHeadRef,
+      3: infraredHeadRef,
+    }),
+    []
+  );
+  const [activeId, setActiveId] = useState(1);
+  const [progressBarHeight, setProgressBarHeight] = useState(0);
   const [activeColor, setActiveColor] = useState('Blue');
   const deviceHeads = ['device_head_blue', 'device_head_red', 'device_head_infrared'].map((key) => colorsData[key]);
   const colorMap = {
@@ -17,6 +32,30 @@ const Colors = ({ colorsData }) => {
     Red: styles.textRed,
     Infrared: styles.textInfrared,
   }[activeColor];
+
+  useEffect(() => {
+    let interval: NodeJS.Timeout;
+    if (isIntervalRunning) {
+      interval = setInterval(() => {
+        if (deviceHeadsListRef.current && refMap[activeId].current) {
+          if (progressBarHeight < refMap[activeId].current.offsetHeight) {
+            setProgressBarHeight((prev) => prev + 1);
+          } else {
+            setProgressBarHeight(0);
+            const nextId = activeId === 3 ? 1 : activeId + 1;
+            setActiveId(nextId);
+            const nextElement = refMap[nextId].current;
+            const scrollPosition = nextElement.offsetLeft - 16;
+            deviceHeadsListRef.current.scrollTo({
+              left: scrollPosition,
+              behavior: 'smooth',
+            });
+          }
+        }
+      }, 100);
+    }
+    return () => interval && clearInterval(interval);
+  }, [progressBarHeight, activeId, refMap, isIntervalRunning]);
 
   return (
     <div className="container md:pb-216 pb-72 overflow-hidden">
@@ -26,16 +65,36 @@ const Colors = ({ colorsData }) => {
         </div>
       </div>
       <div className="md:grid md:grid-cols-12 flex flex-col">
-        <div className="grid md:col-span-4 md:grid-flow-row grid-flow-col md:auto-cols-auto auto-cols-[minmax(300px,_4fr)] md:gap-48 gap-16 overflow-x-auto">
+        <div
+          ref={deviceHeadsListRef}
+          className="grid md:col-span-4 md:grid-flow-row grid-flow-col md:auto-cols-auto auto-cols-[minmax(300px,_4fr)] md:gap-48 gap-16 overflow-x-auto"
+        >
           {deviceHeads.map((deviceHead) => (
             <div
               key={deviceHead.id}
-              className="cursor-pointer"
-              onClick={() => setActiveColor(deviceHead.title.split(' ')[0])}
+              ref={refMap[deviceHead.id]}
+              className={classNames('cursor-pointer relative', activeId === deviceHead.id && styles.active)}
+              onClick={() => {
+                setActiveColor(deviceHead.title.split(' ')[0]);
+                setActiveId(deviceHead.id);
+                setProgressBarHeight(0);
+              }}
+              onMouseEnter={() => setIsIntervalRunning(false)}
+              onMouseLeave={() => setIsIntervalRunning(true)}
             >
-              <p className="text-sm leading-sm">{deviceHead.title}</p>
-              <p className="md:block hidden font-rufina text-xl leading-xl">{deviceHead.short_description}</p>
-              <p className="md:hidden block font-rufina text-xl leading-xl">{deviceHead.short_mobile_description}</p>
+              {activeId === deviceHead.id && (
+                <div
+                  className={classNames('absolute w-4 bg-black-100', styles.progressBar)}
+                  style={{
+                    height: `${progressBarHeight}px`,
+                  }}
+                />
+              )}
+              <div className="ml-6">
+                <p className="text-sm leading-sm">{deviceHead.title}</p>
+                <p className="md:block hidden font-rufina text-xl leading-xl">{deviceHead.short_description}</p>
+                <p className="md:hidden block font-rufina text-xl leading-xl">{deviceHead.short_mobile_description}</p>
+              </div>
             </div>
           ))}
         </div>
