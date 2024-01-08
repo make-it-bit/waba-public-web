778690223c1f4f3ec1d28f5a462714bfd9166184
Sebastian Pikand <sebastianpikand@gmail.com>
2024-01-08 15:13:39 +0200

    added images to product page

--- page-components/Product/MainInfo/MainInfo.tsx
+++ page-components/Product/MainInfo/MainInfo.tsx
@@ -9,16 +9,10 @@ import { getImageFullUrl } from '@/lib/strapi';
 
 const MainInfo = ({ mainInfoData }) => {
   return (
-    <div className="container md:mt-64 mt-24 md:mb-72 mb-64">
-      <div className="grid grid-cols-12 md:gap-y-24 gap-y-32">
-        <div className="md:block hidden col-start-1 col-span-5">
-          <div className="relative w-full h-full min-h-[526px]">
-            <Image src="/device.png" fill className="absolute rotate-90 w-full h-full object-contain" alt="device" />
-            <div className="absolute bg-[#f3ecee] w-full h-full top-0 left-0 z-[-1]"></div>
-          </div>
-        </div>
-
-        <div className="md:hidden block col-span-12">
+    <div className="container lg:mt-64 mt-24 lg:mb-72 mb-64">
+      <div className="grid grid-cols-12 lg:gap-y-24 gap-y-32">
+        {/* mobile images */}
+        <div className="lg:hidden block col-span-12">
           <div className="flex gap-24 overflow-x-auto">
             {mainInfoData.images.data.map((image, index) => (
               <div key={index} className="relative min-w-[260px] h-[260px]">
@@ -33,9 +27,24 @@ const MainInfo = ({ mainInfoData }) => {
           </div>
         </div>
 
-        <div className="md:col-start-7 md:col-span-5 col-span-12 sticky top-[201px]">
+        {/* desktop first image */}
+        {mainInfoData?.images?.data?.[0] && (
+          <div className="lg:block hidden col-start-1 col-span-5">
+            <div className="relative w-full h-full min-h-[526px]">
+              <Image
+                src={getImageFullUrl(mainInfoData.images.data[0])}
+                fill
+                className="absolute w-full h-full object-cover"
+                alt="device"
+              />
+              <div className="absolute bg-[#f3ecee] w-full h-full top-0 left-0 z-[-1]"></div>
+            </div>
+          </div>
+        )}
+
+        <div className="lg:col-start-7 lg:col-span-5 col-span-12 sticky top-[201px]">
           <div className="flex flex-col">
-            <div className="flex flex-wrap gap-8 md:mb-48 mb-32">
+            <div className="flex flex-wrap gap-8 lg:mb-48 mb-32">
               {mainInfoData.tags.data.map((tag, index) => (
                 <Tag
                   key={index}
@@ -45,18 +54,18 @@ const MainInfo = ({ mainInfoData }) => {
               ))}
             </div>
             <h1 className="font-rufina text-4xl leading-4xl">{mainInfoData.title}</h1>
-            <h2 className="text-2xl leading-2xl md:my-8 my-16">{mainInfoData.price}</h2>
-            <p className="text-sm leading-sm md:mt-32 mt-16 md:mb-40 mb-32">{mainInfoData.description}</p>
+            <h2 className="text-2xl leading-2xl lg:my-8 my-16">{mainInfoData.price}</h2>
+            <p className="text-sm leading-sm lg:mt-32 mt-16 lg:mb-40 mb-32">{mainInfoData.description}</p>
             <Checkout mainInfoData={mainInfoData} />
           </div>
         </div>
 
-        <div className="md:block hidden col-start-1 col-span-5">
+        <div className="lg:block hidden col-start-1 col-span-5">
           <div className="grid grid-cols-5 grid-flow-row gap-24">
             <div className="col-start-1 col-span-3 row-span-2">
               <div className="relative w-full h-full min-h-[416px]">
                 <Image
-                  src="/product-main-info-img-1.png"
+                  src={getImageFullUrl(mainInfoData.images.data[1])}
                   fill
                   className="absolute w-full h-full object-cover"
                   alt="product main info image"
@@ -66,7 +75,7 @@ const MainInfo = ({ mainInfoData }) => {
             <div className="col-start-4 col-span-2">
               <div className="relative w-full h-full min-h-[196px]">
                 <Image
-                  src="/product-main-info-img-2.png"
+                  src={getImageFullUrl(mainInfoData.images.data[2])}
                   fill
                   className="absolute w-full h-full object-cover"
                   alt="product main info image"
@@ -76,7 +85,7 @@ const MainInfo = ({ mainInfoData }) => {
             <div className="col-start-4 col-span-2">
               <div className="relative w-full h-full min-h-[196px]">
                 <Image
-                  src="/pre-footer-img-2.png"
+                  src={getImageFullUrl(mainInfoData.images.data[3])}
                   fill
                   className="absolute w-full h-full object-cover"
                   alt="product main info image"
@@ -86,10 +95,14 @@ const MainInfo = ({ mainInfoData }) => {
           </div>
         </div>
 
-        <div className="md:block hidden col-start-1 col-span-5">
+        <div className="lg:block hidden col-start-1 col-span-5">
           <div className="relative w-full h-full min-h-[217px]">
-            <Image src="/device.png" fill className="absolute rotate-90 w-full h-full object-contain" alt="device" />
-            <div className="absolute bg-[#f3ecee] w-full h-full top-0 left-0 z-[-1]"></div>
+            <Image
+              src={getImageFullUrl(mainInfoData.images.data[4])}
+              fill
+              className="absolute w-full h-full object-cover"
+              alt="device"
+            />
           </div>
         </div>
       </div>
