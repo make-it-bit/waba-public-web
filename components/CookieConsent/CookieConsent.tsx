2f430293444ead7848f67bac92ccc4a3f27b94ca
Ketly Iouriev <ketly.iouriev4@gmail.com>
2024-03-08 19:05:36 +0200

    update

--- components/CookieConsent/CookieConsent.tsx
+++ components/CookieConsent/CookieConsent.tsx
@@ -1,6 +1,6 @@
-'use client';
+/* 'use client'; */
 
-import React, { useEffect, useState } from 'react';
+import React /* , { useEffect, useState } */ from 'react';
 import ReactMarkdown from 'react-markdown';
 import rehypeRaw from 'rehype-raw';
 
