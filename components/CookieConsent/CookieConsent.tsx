23f59759f168e189f1c241ee5f68126a63522413
Sebastian Pikand <sebastianpikand@gmail.com>
2024-03-19 12:28:32 +0200

    test

--- components/CookieConsent/CookieConsent.tsx
+++ components/CookieConsent/CookieConsent.tsx
@@ -34,28 +34,28 @@ const CookieConsent = ({ cookiesConsentData }) => {
           <div className="flex flex-col">
             <div className="flex flex-wrap sm:justify-between justify-center items-center gap-x-32 gap-y-16 py-12">
               <div className="text-base leading-base text-neutral-100 sm:text-justify text-center m-0">
-                {cookiesConsentData.read_less_text && (
-                  // <div>{cookiesConsentData.read_less_text}</div>
+                {cookiesConsentData?.read_less_text && (
+                  // <div>{cookiesConsentData?.read_less_text}</div>
                   // REACT MARKDOWN MAKES THE SITE CRASH
-                  <ReactMarkdown rehypePlugins={[rehypeRaw]}>{cookiesConsentData.read_less_text}</ReactMarkdown>
+                  <ReactMarkdown rehypePlugins={[rehypeRaw]}>{cookiesConsentData?.read_less_text}</ReactMarkdown>
                 )}
               </div>
               <div className="flex sm:flex-row flex-col gap-16 sm:w-auto w-full">
                 <Button
-                  CTA={cookiesConsentData.accept_all_button_text}
+                  CTA={cookiesConsentData?.accept_all_button_text}
                   style="tertiary"
                   // onClick={() => setCookieConsent('granted')}
                 />
                 <Button
-                  CTA={cookiesConsentData.accept_necessary_button_text}
+                  CTA={cookiesConsentData?.accept_necessary_button_text}
                   style="quaternary"
                   // onClick={() => setCookieConsent('denied')}
                 />
                 <Button
                   CTA={
                     displayReadMore
-                      ? cookiesConsentData.read_less_button_text
-                      : cookiesConsentData.read_more_button_text
+                      ? cookiesConsentData?.read_less_button_text
+                      : cookiesConsentData?.read_more_button_text
                   }
                   style="quaternary"
                   onClick={() => setDisplayReadMore(!displayReadMore)}
@@ -65,10 +65,10 @@ const CookieConsent = ({ cookiesConsentData }) => {
             {displayReadMore && (
               <div className="w-full">
                 <div className="text-base leading-base text-neutral-100 sm:text-justify text-center my-12">
-                  {cookiesConsentData.read_more_text && (
-                    // <div>{cookiesConsentData.read_more_text}</div>
+                  {cookiesConsentData?.read_more_text && (
+                    // <div>{cookiesConsentData?.read_more_text}</div>
                     // REACT MARKDOWN MAKES THE SITE CRASH
-                    <ReactMarkdown rehypePlugins={[rehypeRaw]}>{cookiesConsentData.read_more_text}</ReactMarkdown>
+                    <ReactMarkdown rehypePlugins={[rehypeRaw]}>{cookiesConsentData?.read_more_text}</ReactMarkdown>
                   )}
                 </div>
               </div>
