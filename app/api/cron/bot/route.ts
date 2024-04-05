62680b0d503b7f65200adf81a74972eeabe8ecf3
Ketly Iouriev <ketly.iouriev4@gmail.com>
2024-04-05 19:02:21 +0300

    added vercel cron job

--- app/api/cron/bot/route.ts
+++ app/api/cron/bot/route.ts
@@ -1,5 +1,4 @@
 export async function GET() {
-  const result = await fetch('http://wabaskin.com');
-  const data = await result.json();
-  return Response.json({ datetime: data.datetime });
+  await fetch('http://wabaskin.com');
+  return Response.json({ message: 'website fetched' });
 }
