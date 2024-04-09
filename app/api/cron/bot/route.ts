fc6c8c96ee8454cdfd8a5f74f3aec83ff10aa939
Ketly Iouriev <ketly.iouriev4@gmail.com>
2024-04-09 17:01:37 +0300

    mini update

--- app/api/cron/bot/route.ts
+++ app/api/cron/bot/route.ts
@@ -11,10 +11,10 @@ export async function GET() {
 
     log.info('Website (http://wabaskin.com) fetched.');
     await log.flush();
-    return NextResponse.json({ message: 'Website fetched.' }, { status: 200 });
+    return NextResponse.json({ message: 'website fetched' }, { status: 200 });
   } catch (error) {
     log.error('Error fetching website (http://wabaskin.com).', { error: error.message });
     await log.flush();
-    return NextResponse.json({ message: 'Error fetching website.' }, { status: 400 });
+    return NextResponse.json({ message: 'error fetching website' }, { status: 400 });
   }
 }
