fcc341212fdc0056263d97bb5d00d20e09a7e946
Ketly Iouriev <ketly.iouriev4@gmail.com>
2024-10-22 15:02:55 +0300

    wabaskin.com to waba.health
    update

--- next.config.js
+++ next.config.js
@@ -7,25 +7,15 @@ module.exports = withAxiom({
   sassOptions: {
     includePaths: [path.join(__dirname, 'app')],
   },
-  /* experimental: {
-      serverActions: true,
-    }, */
+  // experimental: { serverActions: true },
   images: {
     remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'wabaskin.eu',
-        pathname: '**',
-      },
-      {
-        protocol: 'http',
-        hostname: '127.0.0.1',
-        pathname: '**',
-      },
-      {
-        protocol: 'https',
-        hostname: '*.cdninstagram.com',
-      },
+      { protocol: 'https', hostname: 'wabaskin.eu', pathname: '**' },
+      { protocol: 'http', hostname: '127.0.0.1', pathname: '**' },
+      { protocol: 'https', hostname: '*.cdninstagram.com' },
     ],
+  },
+  async redirects() {
+    return [{ source: 'https://www.wabaskin.com', destination: 'https://www.waba.health', permanent: true }];
   },
 });
